5b8466bc7159626aa6130756ccef5db1b93d5a0e
Jinwoo Hwang <201833032@yiu.ac.kr>
2021-10-25 01:28:50 +0900

    초기 커밋.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "도서관 관리";
+
+            label5.Text = DataManager.Books.Count.ToString();
+            label6.Text = DataManager.Users.Count.ToString();
+            label7.Text = DataManager.Books.Where((x) => x.IsBorrowed).Count().ToString();
+            label8.Text = DataManager.Books.Where((x) =>
+                {
+                    return x.IsBorrowed && x.BorrowedAt.AddDays(7) < DateTime.Now;
+                }).Count().ToString();
+
+            dataGridView1.DataSource = DataManager.Books;
+            dataGridView2.DataSource = DataManager.Users;
+        }
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Book book = dataGridView1.CurrentRow.DataBoundItem as Book;
+                textBox1.Text = book.Isbn;
+                textBox2.Text = book.Name;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void dataGridView2_CurrentCellChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                User user = dataGridView2.CurrentRow.DataBoundItem as User;
+                textBox3.Text = user.Id.ToString();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // 대여
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+
+            }
+            else if (textBox3.Text.Trim() == "")
+            {
+
+            }
+            else
+            {
+                try
+                {
+                    Book book = DataManager.Books.Single(x => x.Isbn == textBox1.Text);
+                    if (book.IsBorrowed)
+                    {
+                        MessageBox.Show("이미 대여 중인 도서입니다.");
+                    }
+                    else
+                    {
+                        User user = DataManager.Users.Single(x => x.Id.ToString() == textBox3.Text);
+                        book.UserId = user.Id;
+                        book.UserName = user.Name;
+                        book.IsBorrowed = true;
+                        book.BorrowedAt = DateTime.Now;
+
+                        dataGridView1.DataSource = null;
+                        dataGridView1.DataSource = DataManager.Books;
+                        DataManager.Save();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("존재하지 않는 도서/사용자입니다.");
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("Isbn을 입력해주세요.");
+            }
+            else
+            {
+                try
+                {
+
+                    Book book = DataManager.Books.Single(x => x.Isbn == textBox1.Text);
+                    if (book.IsBorrowed)
+                    {
+                        User user = DataManager.Users.Single(x => x.Id.ToString() == textBox3.Text);
+                        book.UserId = 0;
+                        book.UserName = "";
+                        book.IsBorrowed = false;
+                        book.BorrowedAt = new DateTime();
+
+                        dataGridView1.DataSource = null;
+                        dataGridView1.DataSource = DataManager.Books;
+                        DataManager.Save();
+
+                        if (book.BorrowedAt.AddDays(7) > DateTime.Now)
+                        {
+                            MessageBox.Show("\"" + book.Name + "\"이/가 연체 상태로 반납되었습니다.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("\"" + book.Name + "\"이/가 반납되었습니다.");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("대여 상태가 아닙니다.");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("존재하지 않는 도서 또는 사용자입니다.");
+                }
+            }
+        }
+        private void 도서정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form2().ShowDialog();
+        }
+        private void 사용자정보ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new Form3().ShowDialog();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void menuStrip1_ItemClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/Form3.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+            Text = "사용자 관리";
+
+            dataGridView1.DataSource = DataManager.Users;
+            dataGridView1.CurrentCellChanged += dataGridView1_CurrentCellChanged;
+
+            button1.Click += (sender, e) =>
+            {
+                try
+                {
+                    if (DataManager.Users.Exists((x) => x.Id == int.Parse(textBox1.Text)))
+                    {
+                        MessageBox.Show("사용자 ID가 겹칩니다.");
+                    }
+                    else
+                    {
+                        User user = new User()
+                        {
+                            Id = int.Parse(textBox1.Text),
+                            Name = textBox2.Text
+                        };
+                        DataManager.Users.Add(user);
+
+                        dataGridView1.DataSource = null;
+                        dataGridView1.DataSource = DataManager.Users;
+                        DataManager.Save();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            };
+            button2.Click += (sender, e) =>
+            {
+                try
+                {
+                    User user = DataManager.Users.Single((x) => x.Id == int.Parse(textBox1.Text));
+                    user.Name = textBox2.Text;
+
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = DataManager.Users;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("존재하지 않는 사용자입니다.");
+                }
+            };
+            button3.Click += (sender, e) =>
+            {
+                try
+                {
+                    User user = DataManager.Users.Single((x) => x.Id == int.Parse(textBox1.Text));
+                    DataManager.Users.Remove(user);
+
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = DataManager.Users;
+                    DataManager.Save();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("존재하지 않는 사용자입니다.");
+                }
+            };
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                User user = dataGridView1.CurrentRow.DataBoundItem as User;
+                textBox1.Text = user.Id.ToString();
+                textBox2.Text = user.Name;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void dataGridView1_CurrentCellChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
